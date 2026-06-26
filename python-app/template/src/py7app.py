3104fff489d8e6cc314684f79a717bf8eb2fb79d
aumthakkar <aumthakkar@yahoo.co.uk>
2026-06-26 09:43:07 +0100

    first commit of the backstage-temp-7 repo

---
+++ python-app/template/src/py7app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+import socket
+import time
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')  
+def info():
+    return jsonify({
+        "host": socket.gethostname(),
+        "time": time.ctime(),
+        "env": "dev",
+        "msg": "Hi Pranav!"
+
+    })
+
+@app.route('/api/v1/healthz')  
+def healtz():
+    return jsonify({
+        "status": "up"
+
+    })
+
+if __name__ == '__main__':
+    app.run("0.0.0.0")
+
